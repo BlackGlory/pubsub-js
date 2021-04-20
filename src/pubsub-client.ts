945890f5ed13960f4beab8968107c0109dfad7d4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-04-20 06:15:43 +0000

    refactor: use extra-timers

--- src/pubsub-client.ts
+++ src/pubsub-client.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { ok } from 'extra-response'
 import { assert, CustomError } from '@blackglory/errors'
+import { setDynamicTimeoutLoop } from 'extra-timers'
 
 export { HTTPClientError } from '@blackglory/http-status'
 
@@ -65,22 +66,23 @@ export class PubSubClient {
         observer.error(evt)
       })
 
-      let heartbeatTimer: ReturnType<typeof setInterval> | null = null
+      let cancelHeartbeatTimer: (() => void) | null = null
       if (options.heartbeat ?? this.options.heartbeat) {
         const timeout = options.heartbeat.timeout ?? this.options.heartbeat.timeout
         assert(Number.isInteger(timeout), 'timeout must be an integer')
         assert(timeout > 0, 'timeout must greater than zero')
+
         const probes = options.heartbeat.probes ?? this.options.heartbeat.probes
         assert(Number.isInteger(probes), 'probes must be an integer')
         assert(probes >= 0, 'probes must greater than or equal to zero')
 
         let lastHeartbeat = Date.now()
-        heartbeatTimer = setInterval(() => {
+        cancelHeartbeatTimer = setDynamicTimeoutLoop(timeout, () => {
           if (Date.now() - lastHeartbeat > timeout * (probes + 1)) {
             close()
             observer.error(new HeartbeatTimeoutError())
           }
-        }, options.heartbeat.timeout)
+        })
 
         es.addEventListener('heartbeat', () => lastHeartbeat = Date.now())
       }
@@ -88,7 +90,7 @@ export class PubSubClient {
       return close
 
       function close() {
-        if (heartbeatTimer) clearInterval(heartbeatTimer)
+        if (cancelHeartbeatTimer) cancelHeartbeatTimer()
         es.close()
       }
     })
