b72a842d9bc145f62b499e681df3dcf64da038fb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-01-17 05:03:55 +0000

    feat: add Accept-Version headers
    BREAKING CHANGE: requires PubSub 1.1.0

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { IHTTPOptionsTransformer } from 'extra-request'
-import { url, signal, keepalive, bearerAuth } from 'extra-request/transformers/index.js'
+import { url, signal, keepalive, bearerAuth, header } from 'extra-request/transformers/index.js'
 import { timeoutSignal, raceAbortSignals } from 'extra-abort'
 import type { IPubSubManagerOptions } from './pubsub-manager'
 
@@ -26,6 +26,7 @@ export class PubSubManagerBase {
         )
       ]))
     , keepalive(options.keepalive ?? this.options.keepalive)
+    , header('Accept-Version', '1.1.0')
     ]
   }
 }
